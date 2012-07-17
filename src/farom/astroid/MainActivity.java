f0d6ddc2502268abf499d09f4027c9d1effe34dc
Romain FAFET <farom57@gmail.com>
2012-07-18 00:10:01 +0200

    Custom tab (manque les images)

--- src/farom/astroid/MainActivity.java
+++ src/farom/astroid/MainActivity.java
@@ -1,29 +1,62 @@
 package farom.astroid;
 
 import android.app.TabActivity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TabHost;
+import android.widget.TextView;
+import android.view.LayoutInflater;
+import android.view.View;
 
 
 public class MainActivity extends TabActivity {
 
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//        final TabHost tabHost = getTabHost();
+//
+//        tabHost.addTab(tabHost.newTabSpec("tab1")
+//                .setIndicator("",getResources().getDrawable(R.drawable.icon_connect))
+//                .setContent(new Intent(this, ConnectionActivity.class)));
+//
+//        tabHost.addTab(tabHost.newTabSpec("tab2")
+//                .setIndicator("",getResources().getDrawable(R.drawable.icon_move))
+//                .setContent(new Intent(this, MoveScopeActivity.class)));
+//        
+//        tabHost.setBackgroundDrawable(getResources().getDrawable(R.drawable.btn_red));
+//        
+//
+//    }
+	
+	private TabHost tabHost;
+	 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        final TabHost tabHost = getTabHost();
-
-        tabHost.addTab(tabHost.newTabSpec("tab1")
-                .setIndicator("",getResources().getDrawable(R.drawable.icon_connect))
-                .setContent(new Intent(this, ConnectionActivity.class)));
-
-        tabHost.addTab(tabHost.newTabSpec("tab2")
-                .setIndicator("",getResources().getDrawable(R.drawable.icon_move))
-                .setContent(new Intent(this, MoveScopeActivity.class)));
-        
-        tabHost.setBackgroundDrawable(getResources().getDrawable(R.drawable.btn_red));
-        
-
+        setContentView(R.layout.activity_main);
+ 
+        this.tabHost = getTabHost();
+ 
+        setupTab(getResources().getDrawable(R.drawable.icon_connect), "tab1", new Intent().setClass(this, ConnectionActivity.class));
+        setupTab(getResources().getDrawable(R.drawable.icon_move), "tab2", new Intent().setClass(this, MoveScopeActivity.class));
     }
+ 
+    private void setupTab(Drawable icon, String tag, Intent intent) {
+		tabHost.addTab(tabHost.newTabSpec(tag).setIndicator(createTabView(tabHost.getContext(), icon)).setContent(intent));
+	}
+ 
+	private static View createTabView(final Context context, final Drawable icon) {
+		View view = LayoutInflater.from(context).inflate(R.layout.tab_item, null);
+		ImageView iv = (ImageView) view.findViewById(R.id.tab_icon);
+		iv.setImageDrawable(icon);
+
+ 
+		return view;
+	}
 }
+
