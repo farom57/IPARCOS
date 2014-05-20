5273510a7f48970f530fe975685c18770f3f1388
Romain FAFET <farom57@gmail.com>
2014-05-21 01:08:50 +0200

    Ajout de serveur ok

--- src/farom/astroid/ConnectionActivity.java
+++ src/farom/astroid/ConnectionActivity.java
@@ -1,61 +1,140 @@
 package farom.astroid;
 
+import java.util.List;
+import java.util.Vector;
+
 import android.app.Activity;
 import android.app.ActionBar;
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 import android.os.Build;
 
 public class ConnectionActivity extends Activity {
 
+	private INDIAdapter indiAdapter;
+	private List<String> serverList;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.activity_connection);
 
-		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction().add(R.id.container, new PlaceholderFragment()).commit();
-		}
+		serverList = new Vector<String>();
+		serverList.add(getResources().getString(R.string.hostauto));
+		serverList.add(getResources().getString(R.string.hostadd));
+		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,
+				serverList);
+		dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		((Spinner) findViewById(R.id.spinnerHost)).setAdapter(dataAdapter);
+
+		indiAdapter = INDIAdapter.getInstance();
+		indiAdapter.setLogView((TextView) findViewById(R.id.logTextBox));
+
+		((ToggleButton) findViewById(R.id.connectionButton)).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String host = String.valueOf(((Spinner) findViewById(R.id.spinnerHost)).getSelectedItem());
+				String portStr = ((EditText) findViewById(R.id.editTextPort)).getText().toString();
+				int port;
+				try {
+					port = Integer.parseInt(portStr);
+				} catch (NumberFormatException e) {
+					port = 7624;
+				}
+
+				indiAdapter.connect(host, port);
+			}
+		});
+
+		((Spinner) findViewById(R.id.spinnerHost)).setOnItemSelectedListener(new OnItemSelectedListener() {
+
+			public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+				if (parent.getItemAtPosition(pos).toString().equals(getResources().getString(R.string.hostadd))) {
+					addServer();
+				}
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+				// TODO Auto-generated method stub
+			}
+
+		});
+
+	}
+
+	protected void addServer(String ip) {
+		serverList.add(0,ip);
+		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,
+				serverList);
+		dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		((Spinner) findViewById(R.id.spinnerHost)).setAdapter(dataAdapter);
+	}
+
+	protected void addServer() {
+		// get prompts.xml view
+		LayoutInflater li = LayoutInflater.from(this);
+		View promptsView = li.inflate(R.layout.ip_prompt, null);
+
+		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+		alertDialogBuilder.setView(promptsView);
+
+		final EditText userInput = (EditText) promptsView.findViewById(R.id.editTextDialogUserInput);
+
+		// set dialog message
+		alertDialogBuilder.setCancelable(false).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {
+				addServer(userInput.getText().toString());
+			}
+		}).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {
+				dialog.cancel();
+			}
+		});
+
+		// create alert dialog
+		AlertDialog alertDialog = alertDialogBuilder.create();
+
+		// show it
+		alertDialog.show();
+
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.connection, menu);
 		return true;
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_connection, container, false);
-			return rootView;
-		}
-	}
+	// @Override
+	// public boolean onOptionsItemSelected(MenuItem item) {
+	// // Handle action bar item clicks here. The action bar will
+	// // automatically handle clicks on the Home/Up button, so long
+	// // as you specify a parent activity in AndroidManifest.xml.
+	// int id = item.getItemId();
+	// if (id == R.id.action_settings) {
+	// return true;
+	// }
+	// return super.onOptionsItemSelected(item);
+	// }
 
 }
---
+++ src/farom/astroid/INDIAdapter.java
@@ -0,0 +1,210 @@
+package farom.astroid;
+
+import java.io.IOException;
+import java.util.Date;
+
+import android.widget.TextView;
+import android.widget.ToggleButton;
+import laazotea.indi.Constants.*;
+import laazotea.indi.client.*;
+
+public class INDIAdapter implements INDIDeviceListener, INDIPropertyListener, INDIServerConnectionListener {
+
+	private static final INDIAdapter INSTANCE = new INDIAdapter();
+	private TextView logView;
+	private INDIServerConnection connection;
+	private INDISwitchProperty telescopeMotionNSP;
+	private INDISwitchProperty telescopeMotionWEP;
+	private ToggleButton connectionButton;
+
+	private INDIAdapter() {}
+	
+
+	public static INDIAdapter getInstance(){
+		return INSTANCE;
+	}
+
+	public void connect(java.lang.String host, int port) {
+		log("Try to connect to "+host+":"+port);
+		connection = new INDIServerConnection(host, 7624);
+		connection.addINDIServerConnectionListener(this); // We listen to all
+		new Thread(new Runnable() {
+			public void run() {
+				try {
+					connection.connect();
+					connection.askForDevices(); // Ask for all the devices.
+					log("connection ok");
+//					connectionButton.post(new Runnable() {
+//						 public void run() {
+//							 connectionButton.setActivated(true);
+//						 }
+//						 });
+				} catch (IOException e) {
+					log("Problem with the connection");
+					log(e.getMessage());
+				}
+
+			}
+		}).start();
+	}
+
+
+
+	
+
+//	public void moveNorth() {
+//		try {
+//			telescopeMotionNSP.getElement("MOTION_NORTH").setDesiredValue(SwitchStatus.ON);
+//			telescopeMotionNSP.getElement("MOTION_SOUTH").setDesiredValue(SwitchStatus.OFF);
+//			telescopeMotionNSP.sendChangesToDriver();
+//		} catch (INDIValueException e) {
+//			log(e.getMessage());
+//		} catch (IOException e) {
+//			log(e.getMessage());
+//		}
+//	}
+//
+//	public void moveSouth() {
+//		try {
+//			telescopeMotionNSP.getElement("MOTION_NORTH").setDesiredValue(SwitchStatus.OFF);
+//			telescopeMotionNSP.getElement("MOTION_SOUTH").setDesiredValue(SwitchStatus.ON);
+//			telescopeMotionNSP.sendChangesToDriver();
+//		} catch (INDIValueException e) {
+//			log(e.getMessage());
+//		} catch (IOException e) {
+//			log(e.getMessage());
+//		}
+//	}
+//
+//	public void moveEast() {
+//		try {
+//			telescopeMotionWEP.getElement("MOTION_EAST").setDesiredValue(SwitchStatus.ON);
+//			telescopeMotionWEP.getElement("MOTION_WEST").setDesiredValue(SwitchStatus.OFF);
+//			telescopeMotionWEP.sendChangesToDriver();
+//		} catch (INDIValueException e) {
+//			log(e.getMessage());
+//		} catch (IOException e) {
+//			log(e.getMessage());
+//		}
+//	}
+//
+//	public void moveWest() {
+//		try {
+//			telescopeMotionWEP.getElement("MOTION_WEST").setDesiredValue(SwitchStatus.ON);
+//			telescopeMotionWEP.getElement("MOTION_EAST").setDesiredValue(SwitchStatus.OFF);
+//			telescopeMotionWEP.sendChangesToDriver();
+//		} catch (INDIValueException e) {
+//			log(e.getMessage());
+//		} catch (IOException e) {
+//			log(e.getMessage());
+//		}
+//	}
+//
+//	public void stopMove() {
+//		try {
+//			telescopeMotionWEP.getElement("MOTION_WEST").setDesiredValue(SwitchStatus.OFF);
+//			telescopeMotionWEP.getElement("MOTION_EAST").setDesiredValue(SwitchStatus.OFF);
+//			telescopeMotionNSP.getElement("MOTION_NORTH").setDesiredValue(SwitchStatus.OFF);
+//			telescopeMotionNSP.getElement("MOTION_SOUTH").setDesiredValue(SwitchStatus.OFF);
+//			telescopeMotionNSP.sendChangesToDriver();
+//			telescopeMotionWEP.sendChangesToDriver();
+//		} catch (INDIValueException e) {
+//			log(e.getMessage());
+//		} catch (IOException e) {
+//			log(e.getMessage());
+//		}
+//	}
+
+	
+
+	/**
+	 * @return the logView
+	 */
+	public TextView getLogView() {
+		return logView;
+	}
+
+
+	/**
+	 * @param logView the logView to set
+	 */
+	public void setLogView(TextView logView) {
+		this.logView = logView;
+		log("application started");
+	}
+
+	/**
+	 * @param text The text to display in the log view
+	 */
+	public void log(final String text) {
+		 logView.post(new Runnable() {
+			 public void run() {
+				 logView.append(text+"\n");
+			 }
+		 });
+	}
+
+	@Override
+	public void newDevice(INDIServerConnection connection, INDIDevice device) {
+		// We just simply listen to this Device
+		log("New device: " + device.getName());
+
+		device.addINDIDeviceListener(this);
+	}
+
+	@Override
+	public void removeDevice(INDIServerConnection connection, INDIDevice device) {
+		// We just remove ourselves as a listener of the removed device
+		log("Device Removed: " + device.getName());
+		device.removeINDIDeviceListener(this);
+	}
+
+	@Override
+	public void connectionLost(INDIServerConnection connection) {
+		log("Connection lost. Bye");
+		connectionButton.setActivated(false);
+	}
+
+	@Override
+	public void newMessage(INDIServerConnection connection, Date timestamp, String message) {
+		 log("New Server Message: " + timestamp + " - " + message);
+	}
+
+	@Override
+	public void newProperty(INDIDevice device, INDIProperty property) {
+		// We just simply listen to this Property
+		 log("New Property (" + property.getName() + ") added to device " +
+		 device.getName());
+//		if (property.getName().equals("TELESCOPE_MOTION_NS")) {
+//			telescopeMotionNSP = (INDISwitchProperty) property;
+//			property.addINDIPropertyListener(this);
+//			log("--New Property (" + property.getName() + ") added to device " + device.getName());
+//		}
+//		if (property.getName().equals("TELESCOPE_MOTION_WE")) {
+//			telescopeMotionWEP = (INDISwitchProperty) property;
+//			property.addINDIPropertyListener(this);
+//			log("--New Property (" + property.getName() + ") added to device " + device.getName());
+//		}
+
+	}
+
+	@Override
+	public void removeProperty(INDIDevice device, INDIProperty property) {
+		// We just remove ourselves as a listener of the removed property
+		 log("Property (" + property.getName() + ") removed from device " +
+		 device.getName());
+		property.removeINDIPropertyListener(this);
+	}
+
+	@Override
+	public void messageChanged(INDIDevice device) {
+		 log("New Device Message: " + device.getName() + " - " +
+		 device.getTimestamp() + " - " + device.getLastMessage());
+	}
+
+	@Override
+	public void propertyChanged(INDIProperty property) {
+		 log("Property Changed: " + property.getNameStateAndValuesAsString());
+	}
+
+}
