8794503fdc486f135a709fec89768f28a3c18f61
farom <farom57@gmail.com>
2015-04-09 22:16:50 +0200

    Cleaned and commented SaerchActivity

--- app/src/main/java/farom/iparcos/SearchActivity.java
+++ app/src/main/java/farom/iparcos/SearchActivity.java
@@ -12,13 +12,16 @@ import android.content.Intent;
 import android.widget.SearchView;
 import android.widget.TextView;
 
+/**
+ * Allow the user to search for an astronomical object and display the result.
+ */
+public class SearchActivity extends Activity implements MenuItem.OnActionExpandListener, SearchView.OnQueryTextListener {
 
-public class SearchActivity extends Activity implements MenuItem.OnActionExpandListener {
-    private String query;
-    private MenuItem search_item;
-    private SearchView searchView;
 
-
+    /**
+     * Called at the activity creation. Disable opening animation and load default content.
+     * @param savedInstanceState
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,34 +30,24 @@ public class SearchActivity extends Activity implements MenuItem.OnActionExpandL
 
     }
 
-    @Override
-    protected void onNewIntent(Intent intent) {
-        setIntent(intent);
-        handleIntent(intent);
-
-    }
-
-    private void handleIntent(Intent intent) {
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            query = intent.getStringExtra(SearchManager.QUERY);
-            doMySearch(query);
-            if(search_item!=null){
-                search_item.expandActionView();
-                searchView.setQuery(query,false);
-            }
-        }
-    }
-
-
-
+    /**
+     * perform the search
+     * @param query
+     */
     private void doMySearch(String query) {
 //        while(catalog==null){}
 //        while(!catalog.isReady()){}
 //        catalog.search(query);
-        TextView text=(TextView)findViewById(R.id.textViewtest);
+        TextView text = (TextView) findViewById(R.id.textViewtest);
         text.setText(query);
+        Log.d("GLOBALLOG", "Search for " + query);
     }
 
+    /**
+     * Initiate the menu (only the search view in fact).
+     * @param menu
+     * @return
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the options menu from XML
@@ -63,46 +56,66 @@ public class SearchActivity extends Activity implements MenuItem.OnActionExpandL
 
         // Get the SearchView and set the searchable configuration
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
-        // Assumes current activity is the searchable activity
+
+        SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
         searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        searchView.setIconifiedByDefault(false);
-        searchView.setSubmitButtonEnabled(true);
+        searchView.setIconifiedByDefault(false);    // expand the view at the beginning
+        searchView.setSubmitButtonEnabled(false);   // disable the submit button (the search is done every time the user change the text in the searchView)
+        searchView.setOnQueryTextListener(this);    // to recover text change
 
-        search_item=menu.findItem(R.id.menu_search);
-        search_item.expandActionView();
-        search_item.setOnActionExpandListener(this);
-
+        MenuItem searchItem = menu.findItem(R.id.menu_search);
+        searchItem.expandActionView();              // force expand the view at the beginning (the searchView.setIconifiedByDefault is not working)
+        searchItem.setOnActionExpandListener(this); // recover the collapse event to quit the activity
 
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-
-
-        return super.onOptionsItemSelected(item);
-    }
-
-
+    /**
+     * (from OnActionExpandListener) Called when the search menu is expanded. It can only happens at the menu initialisation. Nothing to do.
+     * @param item
+     * @return
+     */
     @Override
     public boolean onMenuItemActionExpand(MenuItem item) {
         return true;
     }
 
+    /**
+     * (from OnActionExpandListener) Called when the user closes the search menu. It shall kill the activity.
+     * @param item
+     * @return
+     */
     @Override
     public boolean onMenuItemActionCollapse(MenuItem item) {
-        TextView text = (TextView) findViewById(R.id.textViewtest);
-        text.setText("Collapse");
-        Log.d("GLOBALLOG", "Collapse");
         finish();
-        overridePendingTransition(0, 0);
+        overridePendingTransition(0, 0); // Disable the swipe animation for the activity end.
         return true;
     }
 
+
+
+
+    /**
+     * (from OnQueryTextListener) Called when the user changes the search string
+     * @param newText
+     * @return
+     */
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        doMySearch(newText);
+        return false;
+    }
+
+    /**
+     * (from OnQueryTextListener) Called when the user submits the query. Nothing to do since it is done in onQueryTextChange
+     * @param query
+     * @return
+     */
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
 }
+
+
